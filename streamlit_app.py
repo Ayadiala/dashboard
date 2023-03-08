0a5ac96052d59081627d66a6297b7ff24914db43
Ala Eddie Ayadi <36497212+Ayadiala@users.noreply.github.com>
2023-03-08 02:26:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
 
+import pandas as pd
+import streamlit as st
+
+# Create a function to upload the CSV file
+def upload_file():
+    uploaded_file = st.file_uploader("Choose a file", type=['csv'])
+    if uploaded_file is not None:
+        # Save the filename in a variable
+        filename = uploaded_file.name
+        return filename
+    else:
+        return None
+
+# Create the Streamlit app
+def main():
+    st.title("Upload CSV file")
+    filename = upload_file()
+    if filename is not None:
+        st.write("File uploaded: ", filename)
+
+if __name__ == "__main__":
+    main()
