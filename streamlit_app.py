c82b83dc4f82176d0f8d46937b35b1676e9882ed
Ala Eddie Ayadi <36497212+Ayadiala@users.noreply.github.com>
2023-03-08 03:59:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import streamlit as st
 import subprocess
@@ -8,7 +7,7 @@ from langchain.llms import OpenAI
 import openai
 
 
-# Create a function to upload the CSV file
+# Function to upload a CSV file
 def upload_file():
     uploaded_file = st.file_uploader("Choose a file", type=['csv'])
     if uploaded_file is not None:
@@ -18,45 +17,42 @@ def upload_file():
     else:
         return None
 
-# Create the Streamlit app
+
+# Define the Streamlit app
 def main():
-    # Ask the user to specify a variable
-    # Define the OpenAI API key input field in the sidebar
+    # Ask the user to specify an OpenAI API key
     st.title("Oivié-E: Your Instant Text-to-Insights Tool ")
     st.title("Get data-driven insights like never before.")
     st.write("With Oivié-E, you don't need to spend months learning complex analytical software. You can become a data analyst in just a few minutes! ")
-    st.write("Just load your CSV and ask your question about the data , any question you want ! ")
+    st.write("Just load your CSV and ask your question about the data, any question you want! ")
     api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 
-    # Display the variable name
+    # Allow the user to upload a CSV file
     st.write("Upload CSV file")
-    
     filename = upload_file()
     filename_suc = False
     if filename:
         filename_suc = True
 
-    # Set the app header
-    api_key_suc = False
     # Initialize the OpenAI API with the user's inputted API key
+    api_key_suc = False
     if api_key:
-       openai.api_key = api_key
-       os.environ['OPENAI_API_KEY'] = api_key
-       st.write("API key set successfully!")
-       api_key_suc = True
-    
+        openai.api_key = api_key
+        os.environ['OPENAI_API_KEY'] = api_key
+        st.write("API key set successfully!")
+        api_key_suc = True
 
+    # Allow the user to interact with the CSV data through a chatbot
     if filename_suc and api_key_suc:
         st.title('Success ! CHAT with the CSV')
-        st.write('Type a question that you want to know about the data! e.g : how many row in the file? ')
-        st.write('You can ask as much questions as you want , the sky is the limit ( and the 200MB limit ) ')
+        st.write('Type a question that you want to know about the data! e.g : how many rows in the file? ')
+        st.write('You can ask as many questions as you want; the sky is the limit (and the 200MB limit) ')
         user_input = st.text_input('You:', key='input')
 
         if st.button('Send', key='send'):
-
-            # Get the chatbot's response
+            # Use the chatbot to process the user's input
             agent = create_csv_agent(OpenAI(temperature=0), filename, verbose=True)
-            results_st=agent.run(user_input)
+            results_st = agent.run(user_input)
             response = results_st
             # Display the chatbot's response in a text area
             st.text_area('ChatBot:', value=str(response), key='output', height=200)
