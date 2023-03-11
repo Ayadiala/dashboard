8c306ac59c023bd6a5e5ce0e1ff89be78784f109
Ala Eddie Ayadi <36497212+Ayadiala@users.noreply.github.com>
2023-03-11 17:57:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def main():
         os.environ['OPENAI_API_KEY'] = api_key
         try:
             st.write("sarting........!")
-            openai.Completion.create(engine="davinci", prompt="Hello world", max_tokens=5)
+            #openai.Completion.create(engine="davinci", prompt="Hello world", max_tokens=5)
             st.write("API key set successfully!")
             api_key_suc = True
         except openai.error.AuthenticationError:
